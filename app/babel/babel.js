32a51f7616b3200b4ca09dc8644f342729ddb8f5
leiwenzheng <lwz@daibang.com>
2016-04-07 06:48:46 +0800

    babel js

---
+++ app/babel/babel.js
@@ -0,0 +1,17 @@
+/**
+ * babel学习相关js
+ * @author xiaoleizi
+ * @copyright 2016-03-06
+ */
+'use strict';
+let es6 = '2016-03-06';
+console.log(es6);
+let arr = [121,2,3,5];
+var i = 1;
+for(let i in arr) {
+	console.log(arr[i]);
+}
+console.log(i);
+
+
+
---
+++ app/babel/babel_register.js
@@ -0,0 +1,11 @@
+/**
+ * babel-register模块使用
+ * @author xiaoleizi
+ */
+
+//在头步引入babel-register
+console.log(require);
+require("babel-register");
+require("./babel.js");
+
+// console.log(es6);
